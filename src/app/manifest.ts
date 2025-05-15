d6058e4d088a45148a218ab968f0dd7b64637a3c
Matheus Victor <hffmatheus@gmail.com>
2025-05-15 14:21:12 -0300

    background black

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -7,7 +7,7 @@ export default function manifest(): MetadataRoute.Manifest {
     description: 'Track your workouts and progress',
     start_url: '/',
     display: 'standalone',
-    background_color: '#ffffff',
+    background_color: '#000000',
     theme_color: '#000000',
     icons: [
       {
