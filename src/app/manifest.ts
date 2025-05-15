85bf5d8c923036ab9073667b9639091abd2ffc6d
Matheus Victor <hffmatheus@gmail.com>
2025-05-15 14:21:54 -0300

    icon android

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,25 +1,35 @@
-import type { MetadataRoute } from 'next'
- 
+import type { MetadataRoute } from "next";
+
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Gym Hustle',
-    short_name: 'GymHustle',
-    description: 'Track your workouts and progress',
-    start_url: '/',
-    display: 'standalone',
-    background_color: '#000000',
-    theme_color: '#000000',
+    name: "Gym Hustle",
+    short_name: "GymHustle",
+    description: "Track your workouts and progress",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#000000",
+    theme_color: "#000000",
     icons: [
       {
-        src: '/icons/ios/192.png',
-        sizes: '192x192',
-        type: 'image/png',
+        src: "/icons/ios/192.png",
+        sizes: "192x192",
+        type: "image/png",
       },
       {
-        src: '/icons/ios/512.png',
-        sizes: '512x512',
-        type: 'image/png',
+        src: "/icons/ios/512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "/icons/android/android-launchericon-192-192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icons/android/android-launchericon-512-512.png",
+        sizes: "512x512",
+        type: "image/png",
       },
     ],
-  }
+  };
 }
