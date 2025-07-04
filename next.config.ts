6c91f32516a90133be574d449fbe203cd78cf40f
Matheus Victor <hffmatheus@gmail.com>
2025-07-04 14:22:18 -0300

    min

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
-import manifest from './src/app/manifest';
-
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const withPWA = require("next-pwa")({
   disable: process.env.NODE_ENV === "development",
   dest: "public",
   sw: "service-worker.gen.js",
-  manifest,
 });
 
 /** @type {import('next').NextConfig} */
