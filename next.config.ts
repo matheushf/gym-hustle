b72f06550c1dc595851e9193d461be47bf24237b
Matheus Victor <hffmatheus@gmail.com>
2025-07-04 14:02:26 -0300

    chore: Offline and manifest

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
+import manifest from './src/app/manifest';
+
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const withPWA = require("next-pwa")({
   disable: process.env.NODE_ENV === "development",
   dest: "public",
   sw: "service-worker.gen.js",
+  manifest,
 });
 
 /** @type {import('next').NextConfig} */
---
+++ src/app/_offline.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default async function Page() {
+  return (
+    <div>
+      <h1>Gym Hustle is offline</h1>
+      <p>Please check your internet connection and try again.</p>
+    </div>
+  )
+}
